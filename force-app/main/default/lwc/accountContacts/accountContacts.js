366b5291f639dad98b4263248a0070894fed31a1
Karl Manuzon <karl.manuzon@gmail.com>
2020-07-06 18:31:32 -0400

    Display contacts

--- force-app/main/default/lwc/accountContacts/accountContacts.js
+++ force-app/main/default/lwc/accountContacts/accountContacts.js
@@ -1,8 +1,10 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getContactsByAccountId from '@salesforce/apex/AccountContactsController.getContactsByAccountId';
 
 export default class AccountContacts extends LightningElement {
   @api
   recordId;
 
-
+  @wire(getContactsByAccountId, { accountId: '$recordId' })
+  wiredContacts;
 }
